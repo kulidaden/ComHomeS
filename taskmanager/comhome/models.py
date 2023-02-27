0eef3ecdad3ebdfbded698acaaae2e6a35f9785a
Denys <kulidadinis@gmail.com>
2023-02-27 23:41:19 +0200

    cool

--- taskmanager/comhome/models.py
+++ taskmanager/comhome/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title=models.CharField("Ім'я та фамілія", max_length=50)
+    task=models.TextField('Що зацікавило')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name='Задача'
+        verbose_name_plural='Задачи'
+
--- taskmanager/comhome/views.py
+++ taskmanager/comhome/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def index(request):
+    tasks=Task.objects.order_by('-id')
+    return render(request,'menv/index.html',{'title':'Головна сторінка сайту','tasks':tasks})
+
+
+def about(request):
+    return render(request,'menv/about.html')
+
+def create(request):
+    error=''
+    if request.method=='POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error='Форма була не коректною'
+
+
+    form  = TaskForm()
+    context={
+        'form': form,
+        'error': error
+    }
+    return render(request,'menv/create.html',context)
+
