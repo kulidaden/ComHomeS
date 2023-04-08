49ae5ed9c304c8af1626ecb5af4152c6163b8a65
Denys <kulidadinis@gmail.com>
2023-04-08 20:09:02 +0300

    update settings

--- taskmanager/comhome/forms.py
+++ taskmanager/comhome/forms.py
@@ -1,24 +1,40 @@
 from .models import Task
 from django.forms import ModelForm, TextInput, Textarea
-class TaskForm(ModelForm):
+from django.core.exceptions import ValidationError
+from django import forms
+
+
+
+def title_not_str(value):
+    """
+    Ця функція переверіє на коректність заповненого поля title
+    """
+    if not str(value).isalpha():
+        raise ValidationError({'title':"Ім'я та фамілія має містити тільки букви!"})
+
+class TaskForm(forms.ModelForm):
     class Meta:
-        model=Task
-        fields=["title","number","task"]
-        widgets={
+        model = Task
+        task = forms.ChoiceField(choices=Task.task_choice)
+        fields = ["title", "number", "task"]
+        widgets = {
             "title": TextInput(attrs={
-                'class':'form-control',
-                'placeholder':"Введіть ім'я та контактий номер телефону!",
-                'style':"width: 500px;font-size: 20px;height: 50px;"
+                'class': 'form-control',
+                'placeholder': "Введіть ім'я та фамілію",
+                'style': "width: 500px;font-size: 20px;height: 50px;"
 
             }),
             "number": TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введіть номер телефону',
-            'style': "width: 500px; font-size: 20px;margin-top:10px; height: 50px;"
+                'class': 'form-control',
+                'placeholder': 'Введіть номер телефону',
+                'style': "width: 500px; font-size: 20px;margin-top:10px; height: 50px;"
             }),
-            "task": Textarea(attrs={
-                'class':'form-control',
-                'placeholder':'Який пункт Вас зацікавив?',
+            "task": forms.Select(attrs={
+                'class': 'form-control',
+
                 'style': "width: 500px; font-size: 20px;margin-top:10px; height: 50px;"
             }),
         }
+    def clean(self):
+        super().clean()
+        title_not_str(self.cleaned_data.get('title'))
--- taskmanager/comhome/models.py
+++ taskmanager/comhome/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 class Task(models.Model):
     title=models.CharField("Ім'я та фамілія", max_length=50)
     number=models.IntegerField("Номер телефону",validators=[MinValueValidator(1)])
-    task=models.TextField('Що зацікавило')
+    task_choice = [(0, 'Ландшафтний дизайн'), (1, 'Клінінг'), (2, 'Облаштування інтер\'єру'), (3, 'Охоронна система'), (4, 'Дизайн інтер\'єру')]
+    task = models.IntegerField(choices=task_choice)
 
 
-    def str(self):
-        return self.title
+
+    def __str__(self):
+        return f'{self.title} {self.number} {self.task}'
 
     class Meta:
         verbose_name='Задача'
