09f2ecbe6f0b6c4ca5a5a5f7a5fdacd93087b806
Denys <kulidadinis@gmail.com>
2023-04-09 22:35:43 +0300

    add something1

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-from .gdflg import *
+from dotenv import load_dotenv, find_dotenv
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+load_dotenv(find_dotenv())
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = urls_1
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
